9a1e2ede0d37320c3e58ee7cf0716efa66645d0b
karthik <steelsoles@gmail.com>
2017-07-07 17:49:24 +0530

    ajax to referesh map

--- ajaindex.php
+++ ajaindex.php
@@ -12,9 +12,13 @@ define('USER','root');
 define('PASS','123456');
 define('DB','DigiTrack');
 $con = mysqli_connect(HOST,USER,PASS,DB);
+$sql = "SELECT lat, lon
+FROM device
+ORDER BY entry DESC
+LIMIT 1 ";
+$res = mysqli_query($con,$sql);
+$row = mysqli_fetch_array($res)
 
-
-//$result = array();
 ?>
 
 <html>
@@ -23,7 +27,6 @@ $con = mysqli_connect(HOST,USER,PASS,DB);
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <style>
 
     #map {
@@ -40,38 +43,7 @@ $con = mysqli_connect(HOST,USER,PASS,DB);
 </head>
 <body>
   <div class="container-fluid">
-<div class="pannel-yellow">
 
-    <div class="nav-content">
-      <a href="index.php">Home</a>
-
-      <a href="buses.php">Buses</a>
-
-      <a id="route" href="route.php">Routes</a>
-    </div>
-
-  <img class="logo" src="LOGOFinalwhite.png" >
-</div>
-<div style="width:100%; margin-top:17px;">
-  <!--nav ends here-->
-
- <div class=" align-left">
-   <div class="card">
-     <div class="container">
-   <h4><b>Route-name</b></h4>
-   <p>stop1,stop2,stop3</p>
- </div>
-   </div>
-
-   <div class="card">
-     <div class="container">
-   <h4><b>Route-name 2</b></h4>
-   <p>stop1,stop2,stop3?></p>
- </div>
-   </div>
-  </div>
-
-<div class=" align-right ">
   <div id="map"></div>
 
       <script>
@@ -82,38 +54,16 @@ $con = mysqli_connect(HOST,USER,PASS,DB);
             zoom: 2
           });
 
-}
-
-setInterval(   function initmarker() {
-    <?php
-
-    $sql = "SELECT lat, lon
-    FROM device
-    ORDER BY entry DESC
-    LIMIT 1 ";
-    $res = mysqli_query($con,$sql);
-    $row = mysqli_fetch_array($res) ?>
     var marker = new google.maps.Marker({
       position:{lat: <?php echo $row ['lat']; ?>, lng: <?php echo $row ['lon']; ?>},
       map: map,
       title: 'chennai!'
-}); },1000);
+}); }
 
       </script>
       <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCD3JhqHiQ6D1476y-CDHAoFQML9zMhX0w&callback=initMap"
       async defer></script>
 </div>
-</div>
-</div>
-</body>
-<script> $(document).ready(function(){
-      refresh();
-    });
 
-    function refresh(){
-        $('#map').load('ajaindex.php', function(){
-           setTimeout(refresh, 8000);
-        });
-    } </script>
+</body>
 </html>
-<?php mysqli_close($con); ?>
